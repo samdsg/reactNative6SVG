e6b5a1a7dd4323e5ccfbf4d9e3d76d97574dd0eb
Dileepa <chandima@rhino-partners.com>
2019-08-01 11:59:27 +0530

    first commit

---
+++ metro.config.js
@@ -0,0 +1,35 @@
+const { getDefaultConfig, mergeConfig } = require("metro-config");
+
+const svgModule = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false
+        }
+      }),
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
+
+const defaultModule = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
+
+module.exports = mergeConfig(svgModule, defaultModule);
